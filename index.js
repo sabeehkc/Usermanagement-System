e6f267c0e5e974213ad2c575e83120b2a2cb5baa
sabeehkc <wg457129@gmail.com>
2024-01-26 14:33:49 +0530

    session almost completd

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/user_management_system");
 const express = require("express");
 const app = express();
 const nocache = require("nocache")
+
 app.use(express.static('public'))
 
 app.use(nocache())
